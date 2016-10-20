f8b7bae97d3e5e32373bac1cbb843c9a8dbed560
keelii <keeliizhou@gmail.com>
2016-10-20 14:54:20 +0800

    fs

--- themes/demo-theme/static/javascripts/post.js
+++ themes/demo-theme/static/javascripts/post.js
@@ -1,23 +1,72 @@
 (function(win, doc) {
-
-    function fixTOC() {
+    function detectFix() {
         var $toc = $('#side-toc')
         var oTop = $toc.offset().top;
-        var enabled = !$('body').hasClass('collapse-sidebar');
 
-        $(window).bind('scroll.checkToc resize.checkToc', function() {
+        return function() {
             var sWidth = $('.sidebar').width();
             var sTop = $('html').scrollTop() || $('body').scrollTop();
+            var enabled = !$('body').hasClass('collapse-sidebar');
 
             if (sTop > oTop && enabled) {
                 $toc.addClass('toc-fixed').css('width', sWidth);
             } else {
                 $toc.removeClass('toc-fixed').css('width', '100%');
             }
+        }
+    }
+    function fixTOC() {
+        var handler = detectFix();
+
+        $(window).bind('scroll.checkToc resize.checkToc', handler);
+        $(document).delegate('.toggle-sidebar', 'click', handler);
+    }
+
+    function addHotKeys() {
+        $(document).bind('keyup.hotkey', function(event) {
+            var sTop = $('body').scrollTop();
+
+            switch (event.which) {
+                // [s]
+                case 83:
+                    $('.search').focus();
+                    break;
+                // [t]
+                case 84:
+                    $('body').animate({ scrollTop: 0 }, 300);
+                    break;
+                // vim like key
+                // [h]
+                case 72:
+                    var $left = $('.basic-alignment.left');
+                    if ($left.length) {
+                        location.href = $left.attr('href');
+                    }
+                    break;
+                // [l]
+                case 76:
+                    var $right = $('.basic-alignment.right');
+                    if ($right.length) {
+                        location.href = $right.attr('href');
+                    }
+                    break;
+                // [j]
+                case 74:
+                    $('body').animate({ scrollTop: sTop + 300 }, 100);
+                    break;
+                // [k]
+                case 75:
+                    $('body').animate({ scrollTop: sTop - 300 }, 100);
+                    break;
+                default:
+            }
         });
     }
 
-    $(function() {
+    function init() {
+        addHotKeys();
         fixTOC();
-    })
+    }
+
+    $(init);
 })(window, document);
